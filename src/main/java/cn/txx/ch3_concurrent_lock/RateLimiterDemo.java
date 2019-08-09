a6e96d32e68f759568ee7e5020f37d58d01bcce0
tian_xx <1729549607@qq.com>
2019-08-09 21:42:01 +0800

    locksupport

---
+++ src/main/java/cn/txx/ch3_concurrent_lock/RateLimiterDemo.java
@@ -0,0 +1,28 @@
+package cn.txx.ch3_concurrent_lock;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/* *
+ * 描述:
+ * @user tianxinxing
+ * @date 2019/8/9
+ */
+public class RateLimiterDemo {
+    static RateLimiter limiter = RateLimiter.create(2);
+
+    public static class Task implements Runnable{
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis());
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            limiter.acquire();
+            new Thread(new Task()).start();
+        }
+        
+    }
+
+}
